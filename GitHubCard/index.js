59332f897a58af27192e2def4eae96dd2539fff6
Mitchell Wright <mitchellbwright@gmail.com>
2020-06-07 21:48:47 -0600

    initial setup and first axios call to retrieve my own profile

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,13 @@
     https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/mitchellwright')
+  .then( response => {
+    console.table(response);
+  })
+  .catch( error => {
+    console.error(error);
+  });
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
